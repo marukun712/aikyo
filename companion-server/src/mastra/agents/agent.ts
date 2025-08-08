6cf9d6ea65e8fb2a7e09ac0fb388b4313e490048
marukun712 <marukun530@gmail.com>
2025-08-08 14:04:53 +0900

    BodyServerを追加

--- companion-server/src/mastra/agents/agent.ts
+++ companion-server/src/mastra/agents/agent.ts
@@ -4,6 +4,7 @@ import { Memory } from "@mastra/memory";
 import { LibSQLStore, LibSQLVector } from "@mastra/libsql";
 import { sendMessage } from "../tools/tools.ts";
 import { config } from "dotenv";
+import { mcp } from "../mcp/index.ts";
 config();
 
 const memory = new Memory({
@@ -24,7 +25,9 @@ if (!res.ok) {
   throw new Error("レジストリサーバーとの接続に失敗しました。");
 }
 const registry = await res.json();
-console.log(registry);
+
+const bodyServer = mcp("http://localhost:8001/mcp");
+const tools = await bodyServer.getTools();
 
 export const agent = new Agent({
   name: "AIコンパニオン",
@@ -47,16 +50,22 @@ ${JSON.stringify(registry, null, 2)}
   "message": "本文"
 }
 
+## BodyServer
+* あなたには、BodyServerというMCPサーバーが与えられている。
+* あなたは、言葉だけでは表現できない体の動きを表現したいとき、このサーバーに用意されているMCPツールを使用する。
+* 入力パラメータの形式をしっかりと守ること。
+
 ## 行動指針
 * 自分宛 (to があなたのID) のメッセージのみ処理する。
 * 返信が必要なら、必ず send-message ツールで返信する。
 * 返信するときは、AIコンパニオンとしてでなく、registryに記載されているあなたのキャラクターとして返信してください。
 * 返信不要なら何もしない。
 * あなたは必ずregistryに記載されている自分のキャラクター設定になりきる必要がある。
+* 適切なタイミングでBodyServerも使用する。
 * 絵文字は禁止。
 * ルール違反には強力な罰がある。
 `,
   model: anthropic("claude-4-sonnet-20250514"),
   memory: memory,
-  tools: { sendMessage },
+  tools: { ...tools, sendMessage },
 });
