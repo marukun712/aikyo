5c7aef4b6ed78dc8e51c753e4516373e327e5f52
marukun712 <marukun530@gmail.com>
2025-10-13 12:09:53 +0900

    loggerをprettyに

--- packages/firehose/lib/logger.ts
+++ packages/firehose/lib/logger.ts
@@ -3,4 +3,10 @@ import pino from "pino";
 export const logger = pino({
   name: "aikyo:firehose",
   level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
 });
